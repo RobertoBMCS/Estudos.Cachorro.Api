669726ebdc6ec849ef3738be5067ec0092e8f14d
Roberto Botelho <roberto_botelho172@hotmail.com>
2024-02-09 12:13:23 -0300

    feat: application insights local

--- src/Estudos.Cachorro.Api/Program.cs
+++ src/Estudos.Cachorro.Api/Program.cs
@@ -7,12 +7,22 @@ namespace Estudos.Cachorro.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            //builder.Logging.AddApplicationInsights(
+            //    configureTelemetryConfiguration: (config) =>
+            //    {
+            //        config.ConnectionString = builder.Configuration.GetSection("ConnectionsString:ApplicationInsights").Value;
+            //    },
+            //    configureApplicationInsightsLoggerOptions: (options) => { });
 
             var app = builder.Build();
 
